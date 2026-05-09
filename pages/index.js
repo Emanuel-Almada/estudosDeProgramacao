dcce340179c46cd8db75f573565d59eaaca52fc7
Emanuel Almada <emanuelalmada.ti@outlook.com>
2026-05-09 13:36:27 +0000

    alteracao em index.js

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,5 @@
 function Home() {
-  return <h1> "Aprendendo sobre Deploy Next.js e Vercel" </h1>;
-}
-
-function teste() {
-  return <h2>teste feito e aprovado de identação do código</h2>;
+  return <h1> "Aprendendo sobre Deploy Next.js" </h1>;
 }
 
 export default Home;
